3d4ef4c7f249b07deef11001f728f28f93b667ce
Balaji-droid <madhan@softclinch.com>
2026-09-09 18:40:28 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? '/tvs-2-/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
